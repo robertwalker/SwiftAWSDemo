bc1ee9f57b6b884e83da793c91c735524a59b60a
Robert Walker <robertwalker1@icloud.com>
2020-08-20 09:20:50 -0400

    Add extensions to JSONEncoder/JSONDecoder
    The extensions allow encoding and decoding from String rather than Data types.

--- Sources/HelloWorld/main.swift
+++ Sources/HelloWorld/main.swift
@@ -1,5 +1,9 @@
+import Foundation
 import AWSLambdaRuntime
 import AWSLambdaEvents
+
+let jsonEncoder = JSONEncoder()
+let jsonDecoder = JSONDecoder()
 
 struct Input: Codable {
   let number: Double
@@ -11,4 +15,15 @@ struct Output: Codable {
 
 Lambda.run { (context, request: APIGateway.V2.Request, callback: @escaping (Result<APIGateway.V2.Response, Error>) -> Void) in
   callback(.success(APIGateway.V2.Response(statusCode: .ok, body: "Hello World")))
+
+extension JSONEncoder {
+    func encodeAsString<T: Encodable>(_ value: T) throws -> String {
+        try String(decoding: self.encode(value), as: Unicode.UTF8.self)
+    }
+}
+
+extension JSONDecoder {
+    func decode<T: Decodable>(_ type: T.Type, from string: String) throws -> T {
+        try self.decode(type, from: Data(string.utf8))
+    }
 }
